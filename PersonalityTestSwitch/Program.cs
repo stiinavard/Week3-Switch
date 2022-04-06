89c7ad00a632c38527c3bcae4d31cae8ac8b1384
Stiina Vard <stiinavard@gmail.com>
2022-04-06 16:29:24 +0300

    Personality Test

---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm palub kasutajal sisestada tema lemmikvärvi;
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "roheline", konsool kuvab "oled loodusesõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik.";
+
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor)
+            {
+                case "punane":
+                    Console.WriteLine("Oled romantiline.");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled loodusesõber.");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik");
+                    break;
+            }
+
+            Console.Write("Kena päeva!");
+        }
+    }
+}
